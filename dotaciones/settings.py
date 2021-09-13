ac96c706dcea73023d7032e3b3d2335a7b2d836a
Lonnath <lonnath.dev@gmail.com>
2021-09-13 04:16:43 -0500

    Correccion de CORS para acceso de la API.

--- dotaciones/settings.py
+++ dotaciones/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
     'api',
 
 ]
@@ -53,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = 'dotaciones.urls'
